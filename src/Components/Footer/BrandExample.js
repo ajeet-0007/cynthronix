8492bfb704a49a3ef538bd191e9b81dccd812036
Ajeet Gupta <ajeet.gupta@celebaltech.com>
2023-08-04 20:37:37 +0530

    dropdown fixed

--- src/Components/Footer/BrandExample.js
+++ src/Components/Footer/BrandExample.js
@@ -42,7 +42,7 @@
 //                     </NavDropdown.Item>
 //                   </NavDropdown>
 //                 </Nav>
-                
+
 //               </Offcanvas.Body>
 //             </Navbar.Offcanvas>
 //           </Container>
@@ -54,64 +54,91 @@
 
 // export default OffcanvasExample;
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import cynthronixLogo from '../../assets/cynthronix_logo.png'
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import cynthronixLogo from "../../assets/cynthronix_logo.png";
 import { NavLink } from "react-router-dom";
-import './BrandExample.css'
+import "./BrandExample.css";
+import { useState, useRef, useEffect } from "react";
 
 function CollapsibleExample() {
-  return (
-  
-    // <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary bg-dark" >
-    //   <Container className='bg-dark' style={{width: "100%"}}>
-    //     <img src={cynthronixLogo} alt="logo"></img>
-    //     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    //     <Navbar.Collapse id="responsive-navbar-nav">
-    //       <Nav className="me-auto">
-    //         <NavLink to='/' >Our Work</NavLink>
-    //         <NavLink to='/services' >Services</NavLink>
-    //         <NavLink to='/company' >Company</NavLink>
-    //         <NavLink to='/career' >Career</NavLink>  
-    //         <NavLink to='/contact-us' >Contact Us</NavLink>  
-    //       </Nav>
-         
-    //     </Navbar.Collapse>
-    //   </Container>
-    // </Navbar>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary  ticky-top" style={{backgroundColor:"#031e16"}}>
-  <div class="container-fluid " style={{backgroundColor:"#031e16"}}>
-  <img src={cynthronixLogo} alt="logo"></img>
-    <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon "></span>
-      
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-   
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        
-        <li class="nav-item" >
-        <NavLink to='/' class="nav-link active" >Our Work</NavLink>
-        </li>
-        <li class="nav-item" >
-        <NavLink to='/services' class="nav-link active" >Services</NavLink>
-        </li>
-        <li class="nav-item" >
-        <NavLink to='/company' class="nav-link active" >Company</NavLink>
-        </li>
-        <li class="nav-item" >
-        <NavLink to='/career' class="nav-link active">Career</NavLink>  
-        </li>
-        <li class="nav-item" >
-        <NavLink to='/contact-us' class="nav-link active">Contact Us</NavLink>   
-        </li>
-      </ul>
-      
-    </div>
-  </div>
-</nav>
+  const [show, setShow] = useState(false);
+  const navbarRef = useRef()
 
+  const showNavbar = (e) => {
+    setShow(!show);
+  };
+  
+  const showAndHideOutSideDropDown = (event)=>{
+
+  
+      if(event.target.id !== 'dropdown-icon'){
+            
+       navbarRef.current.classList.remove('show');  
+      }
+  }
+ 
+
+  useEffect(()=>{
+
+    window.addEventListener("click", showAndHideOutSideDropDown)
+
+    return ()=>{
+      window.removeEventListener("click", showAndHideOutSideDropDown)
+  }
+},[])
+
+  return (
+    <nav
+      class="navbar navbar-expand-lg bg-body-tertiary  ticky-top"
+      style={{ backgroundColor: "#031e16" }}
+    >
+      <div class="container-fluid " style={{ backgroundColor: "#031e16" }}>
+        <img src={cynthronixLogo} alt="logo"></img>
+        <button
+          className={show ? "navbar-toggler show" : "navbar-toggler"}
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={showNavbar}
+        >
+          <span class="navbar-toggler-icon " id="dropdown-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01" ref={navbarRef}>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <NavLink to="/" class="nav-link active">
+                Our Work
+              </NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink to="/services" class="nav-link active">
+                Services
+              </NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink to="/company" class="nav-link active">
+                Company
+              </NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink to="/career" class="nav-link active">
+                Career
+              </NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink to="/contact-us" class="nav-link active">
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
